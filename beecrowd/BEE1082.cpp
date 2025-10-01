3238bd8626f597d7ab9cfbe210708a4128e72da4
davinoroes <davinoroes1918@gmail.com>
2025-10-01 10:30:18 -0300

    Dama-BEE1087

---
+++ beecrowd/BEE1082.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+bool eh_diagonal(int x,int y,int z,int a){
+    bool value = false;
+    int dif1, dif2;
+    dif1 = abs(x - z);
+    dif2 = abs(y - a);
+    if(dif1 == dif2){
+        value = true;
+    }
+    return value;
+}
+int main(){
+    int x1,x2,y1,y2;
+    while (true){
+        cin >> x1 >> y1 >> x2 >> y2;
+        if(x1 == 0 && x2 == 0 && y1 == 0 && y2 == 0){
+            break;
+        }
+        if(x1==x2 && y1==y2){
+            cout << '0' << endl;
+        }
+        else if(x1 == x2 || y1 == y2){
+            cout << '1' << endl;
+        }
+        else if(eh_diagonal(x1,y1,x2,y2)){
+            cout << '1' << endl;
+        }
+        else{
+            cout << '2' << endl;
+        }
+    }
+    return 0;
+}
