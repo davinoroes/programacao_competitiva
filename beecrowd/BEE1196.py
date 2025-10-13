a6a4ceee5eb83936946cec353a6470df820ef6ca
davinoroes <davinoroes1918@gmail.com>
2025-10-13 13:24:17 -0300

    1196-WERTYU

---
+++ beecrowd/BEE1196.py
@@ -0,0 +1,20 @@
+qwerty_left = {
+    '1': '`', '2': '1', '3': '2', '4': '3', '5': '4', '6': '5',
+    '7': '6', '8': '7', '9': '8', '0': '9', '-': '0', '=': '-',
+    'W': 'Q', 'E': 'W', 'R': 'E', 'T': 'R', 'Y': 'T',
+    'U': 'Y', 'I': 'U', 'O': 'I', 'P': 'O', '[': 'P', ']': '[', '\\': ']',
+    'S': 'A', 'D': 'S', 'F': 'D', 'G': 'F', 'H': 'G',
+    'J': 'H', 'K': 'J', 'L': 'K', ';': 'L', "'": ';',
+    'X': 'Z', 'C': 'X', 'V': 'C', 'B': 'V', 'N': 'B',
+    'M': 'N', ',': 'M', '.': ',', '/': '.', ' ': ' '
+}
+while True:
+    try:
+        text = input()
+        new_word = ''
+        for char in text:
+            if char in qwerty_left:
+                new_word += qwerty_left[char]
+        print(new_word)
+    except EOFError:
+        break
