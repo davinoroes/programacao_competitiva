353bbd95c137a6a481b109cf0ab7fd570fa8b018
davinoroes <davinoroes1918@gmail.com>
2025-10-16 20:20:41 -0300

    Beautiful Year

--- codeforce/271A.cpp
+++ codeforce/271A.cpp
@@ -6,23 +6,16 @@ using namespace std;
 int main(){
     int y;
     cin >> y;
-
     int year = y+1;
-
     while(true){
         string year_string = to_string(year);
-
+        
         set<char> digits(year_string.begin(), year_string.end());
-
         if(year_string.size() == digits.size()){
             cout << year;
             break;
         }
         year++;
     }
-
-
-
-
     return 0;
 }
