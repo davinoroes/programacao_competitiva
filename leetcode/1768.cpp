02d98b420c85aff79d34830c62c39718ee145663
davinoroes <davinoroes1918@gmail.com>
2025-11-04 20:48:35 -0300

    Merge String Alternately

--- leetcode/1768.cpp
+++ leetcode/1768.cpp
@@ -5,15 +5,15 @@ using namespace std;
 string mergeAlternately(string word1, string word2) {
     string merged;
     int i = 0;
-    while(i < word1.size() && i < word2.size()){
+    while(i < int(word1.size()) && i < int(word2.size())){
         merged += word1[i];
         merged += word2[i];
         i++;
     }
-    while(i < word1.size()){
+    while(i < int(word1.size())){
         merged += word1[i];
     }
-    while(i < word2.size()){
+    while(i < int(word2.size())){
         merged += word2[i];
     }
     return merged;
