550ae4c29113f4dd0ccf3d3d8d5c1fd801437546
davinoroes <davinoroes1918@gmail.com>
2025-11-04 18:07:05 -0300

    Carrega ou não Carrega?

---
+++ beecrowd/BEE1026.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> conversor(int a) {
+    vector<int> binary_digits;
+    if (a == 0) {
+        binary_digits.push_back(0);
+    } else {
+        while (a > 0) {
+            binary_digits.insert(binary_digits.begin(), a % 2);
+            a /= 2;
+        }
+    }
+    return binary_digits;
+}
+
+int conversor_bin(vector<int> z) {
+    int result = 0;
+    for (int i = 0; i < int(z.size()); i++) {
+        if (z[int(z.size()) - 1 - i] == 1) {
+            result += (1 << i); // 2^i sem usar pow()
+        }
+    }
+    return result;
+}
+
+vector<int> wrong_sum(vector<int> x, vector<int> y) {
+    vector<int> wrong_result;
+    int max = (x.size() > y.size()) ? x.size() : y.size();
+
+    while (int(x.size()) < max) x.insert(x.begin(), 0);
+    while (int(y.size()) < max) y.insert(y.begin(), 0);
+
+    for (int i = max - 1; i >= 0; i--) {
+        wrong_result.insert(wrong_result.begin(), x[i] ^ y[i]); 
+    }
+    return wrong_result;
+}
+
+int main() {
+    int a, b;
+    while (cin >> a >> b) {
+        vector<int> first = conversor(a);
+        vector<int> second = conversor(b);
+        vector<int> final = wrong_sum(first, second);
+        int result_final = conversor_bin(final);
+        cout << result_final << endl;
+    }
+}
