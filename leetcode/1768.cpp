c95142f7199602eba85c5ed61d6b33894f61f011
davinoroes <davinoroes1918@gmail.com>
2025-11-04 20:29:08 -0300

    Merge Strings Alternately

---
+++ leetcode/1768.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+string mergeAlternately(string word1, string word2) {
+    string merged;
+    int i = 0;
+    while(i < word1.size() && i < word2.size()){
+        merged += word1[i];
+        merged += word2[i];
+        i++;
+    }
+    while(i < word1.size()){
+        merged += word1[i];
+    }
+    while(i < word2.size()){
+        merged += word2[i];
+    }
+    return merged;
+}
+
+int main(){
+    string p1,p2;
+    cin >> p1;
+    cout << "\n";
+    cin >> p2;
+    string final = mergeAlternately(p1,p2);
+    cout << final << endl;
+
+    return 0;
+}
