e5d307683d9d1388f31b16a0e4041b2c183c964c
davinoroes <davinoroes1918@gmail.com>
2025-11-11 14:27:03 -0300

    Reverse Linked List

---
+++ leetcode/206.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+
+
+ListNode* reverseList(ListNode* head) {
+    if (head == NULL || head->next == NULL) return head;
+    ListNode *now, *prox, *ant;
+    now = head, ant = NULL, prox = NULL;
+    while(now != NULL){
+        prox = now->next;
+        now->next = ant;
+        ant = now;
+        now = prox;
+    }     
+    return ant;
+}
+
+
+int main(){
+    return 0;
+}
